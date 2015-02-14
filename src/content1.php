bd6fe1c3fcff42ebbdd3bf89127e2323845d45f9
w-thomas <thomasw@onid.oregonstate.edu>
2015-02-14 12:17:24 -0800

    forgot to add functionality to check for null userames. Fixed now.

--- src/content1.php
+++ src/content1.php
@@ -5,14 +5,26 @@ ini_set('display_errors', 1);
 session_start();
 
 if (isset($_POST['username'])) {
-	$_SESSION['username'] = $_POST['username'];
+	//borrowed from Josh. Checks posted string length to verify
+	//That a name has been entered. If nothing is there it redirects to login.
+	$nameCount = strlen($_POST['username']);
+	if($nameCount < 1) {
+		echo 'A username must be entered.';
+		echo '<br>Click <a href="login.php">here</a>'; 
+		echo ' to return to the login screen';
+		exit();
+	} else {
+		$_SESSION['username'] = $_POST['username'];
+	}
 }
 
+//If no session username has been initialized redirect to login.
 elseif (!isset($_SESSION['username'])) {
 	header('Location: login.php');
 	exit();
 }
 
+//If no session visits have been set start at 0.
 if(!isset($_SESSION['visits'])) {
 		$_SESSION['visits'] = 0;
 }
